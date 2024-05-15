d771c478fb3cd58e6f08e0cc0816172ca25f21ee
victor.longchamps@phibra.com.br <yuri.matos@phibra.com.br>
2024-05-15 18:26:29 -0300

    podio

--- public/js/medalhistas.js
+++ public/js/medalhistas.js
@@ -1,5 +1,7 @@
 const BASE_URL = "http://localhost:8080/";
 
+let players = [];
+let podium = [];
 
 async function getMedalhistas() {
   const response = await fetch(BASE_URL + "medalhistas");
@@ -7,10 +9,34 @@ async function getMedalhistas() {
   return response.json();
 }
 
-async function createCards(data) {
+async function createCards() {
   const wrapper = document.getElementById("wrapper-cards");
 
-  data.forEach((item) => {
+
+  podium.forEach((item) => {
+    const card = document.getElementById("podium-card-" + item.podium);
+    card.classList.add("card-atleta");
+
+
+    card.innerHTML += `
+    <div class="icon">
+      <img class="podium-img" style="width: 100%; height: 200px" src="${item.imagem}" />
+    </div>
+    <div class="info">
+      <span class="atleta-nome">${item.nome}</span>
+      <span class="sport">${item.esporte}</span>
+      <span class="medalhas"> 
+        <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179249.png" />${item.quantidadeDeMedalhas.ouro}</span>
+        <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179251.png" />${item.quantidadeDeMedalhas.prata}</span>
+        <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179250.png" />${item.quantidadeDeMedalhas.bronze}</span>
+      </span>
+    </div>
+    `
+
+
+  })
+
+  players.forEach((item) => {
     const card = document.createElement("div");
     card.classList.add("card-atleta");
     const img = document.createElement("div");
@@ -37,6 +63,9 @@ async function createCards(data) {
 
 addEventListener("load", async () => {
   const response = await getMedalhistas();
-
-  createCards(response);
+  response.forEach((item) => {
+    if (item.podium) podium.push(item);
+    else players.push(item);
+  })
+  createCards();
 })
