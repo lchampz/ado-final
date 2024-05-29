76a633166636e031179198c381586c937ecc4be2
--global <--global>
2024-05-29 16:13:51 -0300

    Creation of the stadiums page

--- public/js/medalhistas.js
+++ public/js/medalhistas.js
@@ -2,6 +2,8 @@ const BASE_URL = "http://localhost:8080/";
 
 let players = [];
 let podium = [];
+let isChartVisible = false;
+let chart = null;
 
 async function getMedalhistas() {
   const response = await fetch(BASE_URL + "medalhistas");
@@ -12,11 +14,9 @@ async function getMedalhistas() {
 async function createCards() {
   const wrapper = document.getElementById("wrapper-cards");
 
-
   podium.forEach((item) => {
     const card = document.getElementById("podium-card-" + item.podium);
     card.classList.add("card-atleta");
-
 
     card.innerHTML += `
     <div class="icon">
@@ -31,41 +31,88 @@ async function createCards() {
         <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179250.png" />${item.quantidadeDeMedalhas.bronze}</span>
       </span>
     </div>
-    `
-
-
-  })
+    `;
+    card.addEventListener("click", () => {
+      createChart(item);
+    });
+  });
 
   players.forEach((item) => {
     const card = document.createElement("div");
     card.classList.add("card-atleta");
-    const img = document.createElement("div");
-    img.classList.add("icon");
 
-    img.style.backgroundImage = `url(${item.imagem})`;
-    card.append(img);
     card.innerHTML += `
+    <div class="icon" style="padding-bottom: 20px">
+      <img class="podium-img" style="width: 100%; height: 200px" src="${item.imagem}" />
+    </div>
     <div class="info">
       <span class="atleta-nome">${item.nome}</span>
       <span class="sport">${item.esporte}</span>
-      <span class="medalhas"> 
+      <span class="medalhas">
         <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179249.png" />${item.quantidadeDeMedalhas.ouro}</span>
         <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179251.png" />${item.quantidadeDeMedalhas.prata}</span>
         <span><img class="medalha" src="https://cdn-icons-png.flaticon.com/512/179/179250.png" />${item.quantidadeDeMedalhas.bronze}</span>
       </span>
     </div>
-    `
-
+    `;
+    card.addEventListener("click", () => {
+      createChart(item);
+    });
     wrapper.append(card);
-  })
+  });
 }
 
+function createChart(arrPlayer) {
+  const name = document.getElementById("player-chart-name");
+  const wrapper = document.getElementById("wrapper-chart");
+  const ctx = document.getElementById("chart");
+  const totalMedalhas = arrPlayer.quantidadeDeMedalhas.ouro +
+    arrPlayer.quantidadeDeMedalhas.prata +
+    arrPlayer.quantidadeDeMedalhas.bronze;
+
+  wrapper.style.display = "flex";
+
+
+  ctx.innerHTML = "";
+  name.innerHTML = `${arrPlayer.nome} - ${totalMedalhas} medalhas`;
+  name.style.color = "black";
+
+  function build() {
+    const data = {
+      labels: ["Ouro", "Prata", "Bronze"],
+      datasets: [
+        {
+          label: "% de Medalhas",
+          data: [
+            (arrPlayer.quantidadeDeMedalhas.ouro / totalMedalhas * 100).toFixed(1),
+            (arrPlayer.quantidadeDeMedalhas.prata / totalMedalhas * 100).toFixed(1),
+            (arrPlayer.quantidadeDeMedalhas.bronze / totalMedalhas * 100).toFixed(1),
+          ],
+          hoverOffset: 4,
+          borderWidth: 1,
+          backgroundColor: ["gold", "rgb(119, 119, 119)", "rgb(253, 177, 78)"],
+        },
+      ],
+    };
+    if (!chart) {
+      chart = new Chart(ctx, {
+        type: "doughnut",
+        data: data,
+      });
+    } else {
+      chart.data = data;
+      chart.update();
+    }
+  }
+
+  build();
+}
 
 addEventListener("load", async () => {
   const response = await getMedalhistas();
   response.forEach((item) => {
     if (item.podium) podium.push(item);
     else players.push(item);
-  })
+  });
   createCards();
-})
+});
